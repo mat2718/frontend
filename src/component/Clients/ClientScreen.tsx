55e6619ec7cbb4718b42316a6c31964fb0dec310
Matthew Hanrahan <sunracer1977@gmail.com>
2021-08-05 11:24:32 -0400

    added a prop from list to screen

--- src/component/Clients/ClientScreen.tsx
+++ src/component/Clients/ClientScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View,Text,StyleSheet, Picker, TouchableOpacity} from 'react-native';
 import GlobalStyles from '../../../assets/constants/GlobalStyles';
 import DemandList from './DemandList';
@@ -7,19 +7,20 @@ import AddDemandScreen from './AddDemandScreen'
 
 
 const ClientScreen=()=>{
+    const[currClient, setClient]=useState()
 
     return(
         <View style={GlobalStyles.container}>
             <View style={styles.clientName}>
-                <Text>Client Name: </Text>
-                
+                <Text>Client Name: </Text>         
                
             <TouchableOpacity style={GlobalStyles.container}>
             <Text style={GlobalStyles.button}>Add</Text>
             </TouchableOpacity>
             </View>
-            <ClientList/>
+            <ClientList setClient={setClient}/>
             <View>
+                <Text>{currClient}</Text>
                <DemandList/>
             </View>
             <TouchableOpacity>
