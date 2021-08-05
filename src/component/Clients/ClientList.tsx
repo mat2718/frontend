ad7a0aef362f57ce273cb0d46ddcd712c3cab1c1
Matthew Hanrahan <sunracer1977@gmail.com>
2021-08-05 11:24:34 -0400

    added a prop from list to screen

--- src/component/Clients/ClientList.tsx
+++ src/component/Clients/ClientList.tsx
@@ -1,29 +1,39 @@
 import React,{useState} from 'react'
-import {View,Text,Picker,StyleSheet} from 'react-native'
+import {View,Text,StyleSheet} from 'react-native'
 import GlobalStyles from '../../../assets/constants/GlobalStyles'
-const name="Bob"
+import RNPickerSelect from 'react-native-picker-select';
 
 
 
-const ClientList=()=>{
-    const [state,setState]=useState(name);
-
+const ClientList=(props: { setClient: (arg0: any) => void; })=>{
+    
 
     return(
         <View style={GlobalStyles.container}>
-            <Picker
+            <RNPickerSelect
+            onValueChange={(value) => props.setClient(value)}
+            items={[
+                {label:"WebStuff", value:"webstuff"},
+                {label:"InfoMe", value:"infome"},
+                {label:"CogWheel", value:"cogwheel"},
+                {label:"NoAWS" ,value:"noaws"},
+                {label:"RevatureJr", value:"revaturejr"}
+            ]}
+        />
+
+            {/* <Picker
                 style={styles.drop}
                 selectedValue={state}
                
             >
-                <Picker.Item label="WebStuff" value="webstuff"/>
-                <Picker.Item label="InfoMe" value="infome"/>
-                <Picker.Item label="CogWheel" value="cogwheel"/>
-                <Picker.Item label="NoAWS" value="noaws"/>
-                <Picker.Item label="REvatureJr" value="revaturejr"/>
+                <Picker.Item {label="WebStuff", value="webstuff"}/>
+                <Picker.Item {label="InfoMe", value="infome"}/>
+                <Picker.Item {label="CogWheel", value="cogwheel"}/>
+                <Picker.Item {label="NoAWS" ,value="noaws"}/>
+                <Picker.Item {label="REvatureJr", value="revaturejr"}/>
 
 
-            </Picker>
+            </Picker> */}
         </View>
 
 
