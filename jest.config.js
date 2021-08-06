428b2638be80e2e3f77a9b3fdafed5d024d4bf80
Red Oral <redmunozoral@gmail.com>
2021-08-06 15:56:09 -0700

    Added necessary transformIgnorePatterns to fix testing.

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,9 @@ const { withEnzyme } = require('jest-expo-enzyme');
 
 const addSetup = (config) => {
   config.setupFilesAfterEnv.push('<rootDir>/__tests__/setup.js');
+  config.transformIgnorePatterns = [
+    'node_modules/(?!(jest-)?(react-native|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@react-native-picker|react-native-picker-select|react-native-chart-kit|react-native-calendars|react-native-swipe-gestures|expo-font|expo-asset|expo-constants)/)',
+  ];
   return config;
 };
 
