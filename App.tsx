bfd1523467e79ed5421366e00fd1aab8e01c1f99
imranilyas <68575807+imranilyas@users.noreply.github.com>
2021-08-09 12:07:48 -0700

    Material Icons Import
    icon import added to json

--- App.tsx
+++ App.tsx
@@ -1,14 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import ViewFlatList from './src/components/Trainer/ViewFlatList';
+import ViewFlatList from './src/Components/Trainer/ViewFlatList';
 import CreateTrainer from './src/Screens/CreateTrainer';
+import Header from './src/Components/Header';
+import EditTrainer from './src/Components/Trainer/EditTrainer';
+import SearchBar from './src/Components/Trainer/Searchbar';
 
 export default function App() {
   const str = [{name: 'Johnathan Jingles', ID: '87654'}, {name: 'chup', ID: '87774'}]
+  const trainer = {FirstName: 'Johnson', LastName: 'Falls', Email: 'jfalls@rev.net', ID: '42987429'};
   return (
+
     <View style={styles.container}>
-      <ViewFlatList trainerArr={str}/>
+    <ViewFlatList trainerArr={str}/>
+      {/* <CreateTrainer/> */}
+      {/* <EditTrainer trainer = {trainer}/> */}
+      {/* <SearchBar/> */}
     </View>
   );
 }
