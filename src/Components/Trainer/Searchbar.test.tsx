287051ce52a0adadb10e798b115b5f961391baf4
imranilyas <68575807+imranilyas@users.noreply.github.com>
2021-08-09 12:08:43 -0700

    SearchBar start

---
+++ src/Components/Trainer/Searchbar.test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { shallow, mount } from "enzyme";
+import SearchBar from "./Searchbar";
+
+describe('', () => {
+    it('', () => {
+
+    })
+})
--- src/Components/Trainer/Searchbar.tsx
+++ src/Components/Trainer/Searchbar.tsx
@@ -1,6 +1,33 @@
-import React from 'react'
-import {Text, View, KeyboardAvoidingView, Keyboard, TouchableWithoutFeedback} from 'react-native';
+import React, {useState} from 'react'
+import {Text, View, StyleSheet, KeyboardAvoidingView, Keyboard, TouchableWithoutFeedback, TextInput, TouchableOpacity} from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
 //import axios from 'axios'
 /**
  * Authors: Joab Smith and Imran Ilyas
 **/
+const SearchBar = () => {
+    const [search, setSearch] = useState('');
+    const searched = () => {
+        return('searching for a trainer');
+    }
+    return(
+        <View style = {styles.container}>
+            <TextInput style = {styles.searchBar} placeholder='Search Trainers' onChangeText={setSearch}>{search}</TextInput>
+            {/* <SearchIcon style = {styles.searchIcon}/> */}
+            <TouchableOpacity onPress={searched}>
+                <MaterialCommunityIcons name="magnify" color={'grey'} size={30} />
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+    },
+    searchBar: {},
+    searchIcon: {},
+})
+
+export default SearchBar;
