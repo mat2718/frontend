5b015c3e5060af7b8fe0da1a7da6d600e830c5ab
Red Oral <82777516+redoral@users.noreply.github.com>
2021-08-11 14:01:12 -0700

    Added more tests on the Batches screens and components.

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,25 @@
 const { withEnzyme } = require('jest-expo-enzyme');
 
 module.exports = {
-  projects: [
-    addConfig(withEnzyme(require('jest-expo/android/jest-preset'))),
-  ],
+  projects: [addConfig(withEnzyme(require('jest-expo/android/jest-preset')))],
   collectCoverage: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx,js,jsx}'],
+  collectCoverageFrom: [
+    '<rootDir>/src/Screens/Batches/*.{ts,tsx,js,jsx}',
+    '<rootDir>/src/Screens/ViewBatch/*.{ts,tsx,js,jsx}',
+    '<rootDir>/src/Screens/AddEditBatch/*.{ts,tsx,js,jsx}',
+  ],
   coverageDirectory: 'coverage',
   coverageThreshold: {
     global: {
       statements: 70,
     },
   },
-}
-
+};
 
 /**
  * Returns an array with a single string that tells Babel to ignore uncompiled
  * third-party React Native libraries.
- * 
+ *
  * @param {*} ignoreThese array of strings representing third-party libraries in node_modules
  * @returns array with a single string element
  */
@@ -26,7 +27,7 @@ function returnTransformIgnorePatterns(ignoreThese) {
   const start = 'node_modules/(?!(jest-)?(';
   const end = ')/)';
   let str = start;
-  for(const i in ignoreThese) {
+  for (const i in ignoreThese) {
     if (+i === 0) {
       str += `${ignoreThese[i]}`;
     } else {
@@ -37,26 +38,27 @@ function returnTransformIgnorePatterns(ignoreThese) {
   return [str];
 }
 
-
 /**
- * `withEnzyme` comes with a lot of useful configuration to save you time, 
- * but sometimes you need a little more. This function will modify the object 
- * returned by `withEnzyme` to suit the needs of our project. 
- * 
+ * `withEnzyme` comes with a lot of useful configuration to save you time,
+ * but sometimes you need a little more. This function will modify the object
+ * returned by `withEnzyme` to suit the needs of our project.
+ *
  * @param {*} config the object returned by `withEnzyme`
  * @returns the same object but with modified properties
  */
 function addConfig(config) {
   // add extra setup file
   config.setupFilesAfterEnv.push('<rootDir>/__tests__/setup.js');
-  
+
   // comment this out if you want to test all files
   config.testMatch = [
-    '<rootDir>/src/Components/Clients/__tests__/AddClient.test.js',
+    '<rootDir>/src/Screens/Batches/*.test.{ts,tsx,js,jsx}',
+    '<rootDir>/src/Screens/ViewBatch/*.test.{ts,tsx,js,jsx}',
+    '<rootDir>/src/Screens/AddEditBatch/*.test.{ts,tsx,js,jsx}',
   ];
 
   // third-party libraries that throw errors
-  // see https://jestjs.io/docs/tutorial-react-native#transformignorepatterns-customization 
+  // see https://jestjs.io/docs/tutorial-react-native#transformignorepatterns-customization
   const ignoreThese = [
     'react-native',
     '@react-native-community',
@@ -80,8 +82,8 @@ function addConfig(config) {
   ];
 
   console.log(
-    'transformIgnorePatterns set to: \n', 
-    returnTransformIgnorePatterns(ignoreThese)+'\n'
+    'transformIgnorePatterns set to: \n',
+    returnTransformIgnorePatterns(ignoreThese) + '\n'
   );
 
   config.transformIgnorePatterns = returnTransformIgnorePatterns(ignoreThese);
--- src/components/batches/BatchListItem/BatchListItem.test.tsx
+++ src/components/batches/BatchListItem/BatchListItem.test.tsx
@@ -1,38 +1,84 @@
 import React from 'react';
-import { mount } from 'enzyme';
-import { BarChart } from 'react-native-chart-kit';
+import { mount, shallow } from 'enzyme';
 import BatchListItem from './';
-import { TouchableOpacity } from 'react-native';
+import { TouchableOpacity, Text } from 'react-native';
 
-let wrapper: any;
+const today = new Date();
+const yesterday = new Date(today);
+const tomorrow = new Date(today);
+
+yesterday.setDate(yesterday.getDate() - 1);
+tomorrow.setDate(tomorrow.getDate() + 1);
+
+const mockNavigate = jest.fn();
+jest.mock('@react-navigation/native', () => {
+  return {
+    ...jest.requireActual('@react-navigation/native'),
+    useNavigation: () => {
+      return {
+        navigate: mockNavigate,
+      };
+    },
+  };
+});
+
+let wrap: any;
+
+/** props for the component */
+const props = {
+  associate: 25,
+  batchId: 0,
+  curriculum: 'Cloud Native',
+  trainer: 'Robert Connell',
+  startDate: 1622505600000,
+  endDate: 1627776000000,
+};
 
 describe('Batches', () => {
   beforeEach(() => {
-    wrapper = mount(
-      <BatchListItem
-        associate={25}
-        batchId={0}
-        curriculum='Cloud Native'
-        trainer='Robert Connell'
-        startDate={1622505600000}
-        endDate={1627776000000}
-      />
-    );
+    wrap = mount(<BatchListItem {...props} />);
   });
 
-  // tests if the component is there
+  /** tests if the component is there */
   it('should be there', () => {
-    expect(wrapper).not.toBe(undefined);
+    expect(wrap).not.toBe(undefined);
   });
 
-  // tests if the barchart is defined
+  /** tests the button */
   it('should be pressable', () => {
-    const shouldBePressed = wrapper.find(TouchableOpacity).at(0);
+    const shouldBePressed = wrap.find(TouchableOpacity).at(0);
     const myEventHandler = jest.spyOn(shouldBePressed.props(), 'onPress');
 
     const actualEventHandler = shouldBePressed.prop('onPress');
     actualEventHandler();
 
     expect(myEventHandler).toHaveBeenCalled();
+  });
+
+  /** tests the navigate button */
+  it('pressing the button navigates to new screen', () => {
+    let wrapper = shallow(<BatchListItem {...props} />);
+    let button = wrapper.find({ testID: 'button' }).last();
+    button.invoke('onPress')();
+    expect(mockNavigate).toHaveBeenCalledWith('ViewBatch', props);
+  });
+
+  /** tests the if statements */
+  it('should test the if statements', () => {
+    wrap = mount(
+      <BatchListItem
+        associate={20}
+        batchId={1}
+        curriculum='React Native'
+        trainer='Matthew Otto'
+        startDate={yesterday.getTime()}
+        endDate={tomorrow.getTime()}
+      />
+    );
+
+    let displays = wrap
+      .find(Text)
+      .someWhere((node: any) => node.text().includes('Active'));
+    expect(displays).toBeTruthy();
   });
 });
