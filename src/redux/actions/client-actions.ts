95c8eb9d9b9d1561f2e801a1bbdfb58aaeb4b65f
Matthew Hanrahan <sunracer1977@gmail.com>
2021-08-13 12:58:36 -0400

    fixed client calls

--- src/redux/actions/client-actions.ts
+++ src/redux/actions/client-actions.ts
@@ -18,12 +18,35 @@ export const getAllClients = async (dispatch: Dispatch) => {
 export const getClientByName =
   (clientName: string) => async (dispatch: Dispatch) => {
     try {
-      const res = await axios.get(`/client/id/${clientName}`);
-      dispatch({
-        type: AppActions.UPDATE_CLIENT,
-        payload: res.data,
-      });
+      const res = await axios.get(`client/name/${clientName}`);      
+     return (res.data)
+      
     } catch (e) {
       console.log(e);
     }
   };
+/** Gets one client by ID*/
+export const getClientByID =
+  (clientID: number) => async (dispatch: Dispatch) => {
+    try {
+      const res = await axios.get(`client/id/${clientID}`);     
+       return (res.data)
+      
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  
+/** adds a client */
+export const addClient = 
+(client:string) => async (dispatch: Dispatch) => {
+    try {
+       const res =await axios.post('client',{
+          clientname:client});
+       return(res.data)
+
+          
+       `` }catch (e) {
+              console.log(e)
+          }
+        };
