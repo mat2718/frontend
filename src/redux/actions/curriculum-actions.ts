fc02d40639afaa0fc6db647ef91dc06aef9ddde4
j-m-smith426 <joabsmith4226@gmail.com>
2021-08-13 12:47:05 -0400

    minor fixes

--- src/redux/actions/curriculum-actions.ts
+++ src/redux/actions/curriculum-actions.ts
@@ -1,69 +1,72 @@
-import axios from '../../../axiosConfig';
-import { Dispatch } from 'redux';
-import ICurriculum from '../../entities/curriculum';
-import { AppActions } from './actions';
-
+import axios from "../../../axiosConfig";
+import { Dispatch } from "redux";
+import ICurriculum from "../../entities/curriculum";
+import { AppActions } from "./actions";
 
 //api call for getting all curricula
 export const GetAllCurricula = () => async (dispatch: Dispatch) => {
-    try {
-        const res = await axios.get('/curriculum');
-        dispatch({
-            type: AppActions.UPDATE_CURRICULA,
-            payload: res.data
-        });
-    } catch (err) {
-        console.log(err)
-    }
+  try {
+    const res = await axios.get("/curriculum");
+    dispatch({
+      type: AppActions.UPDATE_CURRICULA,
+      payload: res.data,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 /**api call for getting a specific curriculum by ID
- * 
+ *
  * @param id
- * 
-*/
+ *
+ */
 
 export const GetCurriculum = (id: number) => async (dispatch: Dispatch) => {
-    try {
-        const res = await axios.get(`/curriculum/id/${id}`);
-        dispatch({
-            type: AppActions.UPDATE_CURRICULA,
-            payload: res.data
-        });
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    return await axios.get(`/curriculum/id/${id}`);
+    // dispatch({
+    //     type: AppActions.UPDATE_CURRICULA,
+    //     payload: res.data
+    // });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 //api call to post a new curriculum
-export const PostCurriculum = (curriculum: {}) => async(dispatch: Dispatch) => {
-    try {
-        await axios.post('/curriculum', curriculum);
-        const res = await axios.get('/curriculum');
-        dispatch({
-            type: AppActions.UPDATE_CURRICULA,
-            payload: res.data
-        });
-    } catch (err) {
-        console.log(err)
-    }
+export const PostCurriculum = (curriculum: {}) => async (
+  dispatch: Dispatch
+) => {
+  try {
+    await axios.post("/curriculum", curriculum);
+    GetAllCurricula();
+    // const res = await axios.get('/curriculum');
+    // dispatch({
+    //     type: AppActions.UPDATE_CURRICULA,
+    //     payload: res.data
+    // });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 /**api call for deleting a specific curriculum by ID
- * 
+ *
  * @param id
- * 
-*/
-export const DeleteCurriculum = (id: number) => async(dispatch: Dispatch) => {
-    try {
-        await axios.delete(`/curriculum/id/${id}`);
-        const res = await axios.get('/curriculum');
-        dispatch({
-            type: AppActions.UPDATE_CURRICULA,
-            payload: res.data
-        })
-        return 'Curriculum has been deleted.';
-    } catch (err) {
-        console.log(err);
-    }
+ *
+ */
+export const DeleteCurriculum = (id: number) => async (dispatch: Dispatch) => {
+  try {
+    await axios.delete(`/curriculum/id/${id}`);
+    GetAllCurricula();
+    // const res = await axios.get('/curriculum');
+    // dispatch({
+    //     type: AppActions.UPDATE_CURRICULA,
+    //     payload: res.data
+    // })
+    return "Curriculum has been deleted.";
+  } catch (err) {
+    console.log(err);
+  }
 };
