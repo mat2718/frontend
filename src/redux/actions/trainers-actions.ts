0d35ddb9ed32a784508bf2fb2e6291bfe32d9e93
j-m-smith426 <joabsmith4226@gmail.com>
2021-08-15 15:09:45 -0400

    testing update

--- src/redux/actions/trainers-actions.ts
+++ src/redux/actions/trainers-actions.ts
@@ -5,6 +5,7 @@ import { AppActions, IAppAction } from "./actions";
 import { IAppState } from "../state";
 import { useDispatch, useSelector } from "react-redux";
 import { AxiosResponse } from "axios";
+import { ThunkDispatch } from "redux-thunk";
 
 /**
  * Trainer Handler - Axios Requests to the Database regarding the Trainer
@@ -14,15 +15,17 @@ import { AxiosResponse } from "axios";
  */
 
 //Handler for adding a trainer to the database
-export const addTrainer = (trainer: ITrainer) => async (
-  dispatch: Dispatch<IAppAction>
-) => {
+export const addTrainer = (trainer: ITrainer) => async (dispatch:Dispatch) => {
   try {
     await axios.post("trainer", trainer);
-    (() => {
-      const dispatcher = useDispatch();
-      dispatcher(getAllTrainers());
-    })();
+     const trainers:ITrainer[] = await axios.get("trainer").then((response) => response.data)
+      
+          console.log("resp", trainers);
+          dispatch({
+              type: AppActions.UPDATE_TRAINER,
+              payload: { trainers, },
+          });
+
     return `${trainer.trainerfirst} ${trainer.trainerlast} has been added`;
   } catch (err) {
     return "Error when attempting to add a trainer: " + err;
@@ -44,21 +47,14 @@ export const deleteTrainer = (trainer: ITrainer) => async () => {
 };
 
 //Handler for retrieving all trainers from the database
-export const getAllTrainers = () => async (dispatch: Dispatch<IAppAction>) => {
+export const getAllTrainers = () => async (dispatch: Dispatch) => {
   try {
     await axios.get("trainer").then((response) => {
       const trainers: ITrainer[] = response.data;
       console.log("resp", response.data, trainers);
       dispatch({
         type: AppActions.UPDATE_TRAINER,
-        payload: {
-          skills: [],
-          clients: [],
-          batches: [],
-          curricula: [],
-          demands: [],
-          trainers,
-        },
+        payload: { trainers, },
       });
       return "Retrieved all Trainers";
     });
@@ -68,13 +64,12 @@ export const getAllTrainers = () => async (dispatch: Dispatch<IAppAction>) => {
 };
 
 //Handler for updating a trainer's information
-export const updateTrainer = (trainer: ITrainer) => async () => {
+export const updateTrainer = (trainer: ITrainer) => async (dispatch:any) => {
   try {
     await axios.put("trainer", { ...trainer, curriculaIdArr: [] });
-    (() => {
-      const dispatcher = useDispatch();
-      dispatcher(getAllTrainers());
-    })();
+
+      dispatch(getAllTrainers());
+  
   } catch (err) {
     return (
       `Unable to update ${trainer.trainerfirst} ${trainer.trainerlast}'s information: ` +
@@ -97,7 +92,7 @@ export const getTrainerFName = (trainerName: string) => async () => {
 
 // Handler for getting a trainer by their last name
 export const getTrainerLName = (trainerName: string) => async (
-  dispatch: Dispatch<IAppAction>
+  dispatch: Dispatch
 ) => {
   try {
     const response: AxiosResponse = await axios.get(
@@ -111,7 +106,7 @@ export const getTrainerLName = (trainerName: string) => async (
 
 // Handler for getting a trainer by their ID number
 export const getTrainerId = (trainerId: number) => async (
-  dispatch: Dispatch<IAppAction>
+  dispatch: Dispatch
 ) => {
   try {
     const response: AxiosResponse = await axios.get(`trainer/id/${trainerId}`);
