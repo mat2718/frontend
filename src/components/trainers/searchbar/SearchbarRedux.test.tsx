ce613287af8fa88c5202fa97fb4cad6fd53d7997
Kai <kaiyip.ho23@gmail.com>
2021-08-17 18:24:54 -0400

    Revert "Merge branch 'dev-branch' into dev-diagram-stretch"
    This reverts commit d84f9345f652023731b6ebeff96375f0129d095c, reversing
    changes made to 654ec2ddc5b7ddcba4e3e5a16fb73aac933d60bf.

--- src/components/trainers/searchbar/SearchbarRedux.test.tsx
+++ src/components/trainers/searchbar/SearchbarRedux.test.tsx
@@ -8,7 +8,6 @@ import SearchBar from ".";
 import { Reducer } from "../../../redux/reducer";
 import { Picker } from '@react-native-picker/picker';
 import { TouchableOpacity } from 'react-native';
-import { Provider as PaperProvider } from 'react-native-paper';
 
 
 jest.requireActual('react-redux');
@@ -23,13 +22,9 @@ describe('Search Bar button', () =>
     );
     const mock = jest.fn();
     const wrapper = mount(
-      <PaperProvider>
-        <Provider store={mockStore}>
-          <SearchBar setTrainer={mock} />
-        </Provider>
-      </PaperProvider>
-    );
-      
+      <Provider store={mockStore}>
+       <SearchBar setTrainer={mock} />
+      </Provider>);
     
     wrapper.find('TouchableOpacity').findWhere(node=>node === node).first().invoke('onPress')();
     
