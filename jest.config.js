f860ca469369a5892cd65a7d927edd29cf856258
Kai <kaiyip.ho23@gmail.com>
2021-08-17 13:01:04 -0400

    test

--- jest.config.js
+++ jest.config.js
@@ -82,10 +82,11 @@ function addConfig(config) {
   ];
 
   /* comment this out if you want to test all files */
-  config.testMatch = [
-    '<rootDir>/src/components/confirm-dialog/**/*.test.[jt]s?(x)',
+  // config.testMatch = [
+    // '<rootDir>/src/components/confirm-dialog/**/*.test.[jt]s?(x)',
     // '<rootDir>/src/components/curricula/expandable-list/**/*.test.tsx',
-  ];
+    // '<rootDir>/src/screens/diagram/**/*.test.[jt]s?(x)'
+  // ];
 
   // third-party libraries that throw errors
   // see https://jestjs.io/docs/tutorial-react-native#transformignorepatterns-customization
