cb90c49321959322465ce74afaf0011192b7486d
calebmsword <caleb.m.sword@gmail.com>
2021-08-18 11:55:53 -0400

    all tests pass :)

--- jest.config.js
+++ jest.config.js
@@ -4,9 +4,7 @@ module.exports = {
   projects: [addConfig(withEnzyme(require('jest-expo/android/jest-preset')))],
   collectCoverage: true,
   collectCoverageFrom: [
-    '!<rootDir>/src/**/*.{ts,tsx,js,jsx}',
-    '!<rootDir>/src/components/clients_old/**/*',
-    '<rootDir>/src/screens/view-batch/**/*',
+    '<rootDir>/src/**/*.{ts,tsx,js,jsx}',
     '!<rootDir>/src/assets/**/*',
     '!<rootDir>/src/types.ts',
     '!<rootDir>/src/entities/*',
